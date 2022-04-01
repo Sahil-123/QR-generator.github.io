9e4bd95686501e01383bd32bce41aa21e1e3848d
Sahil-123 <58927647+Sahil-123@users.noreply.github.com>
2022-04-01 23:04:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let btn=document.getElementById("btn");
-let img=document.getElementById("img");
-let loader=document.getElementById("loader");
+let btn = document.getElementById("btn");
+let img = document.getElementById("img");
+let loader = document.getElementById("loader");
 
-btn.addEventListener("click",()=>{
-    loader.style.display="block";
+btn.addEventListener("click", () => {
+    loader.style.display = "block";
     console.log("pressed");
 
-    img.onload=()=>{
-        loader.style.display="none";
+    img.onload = () => {
+        loader.style.display = "none";
         console.log("loaded");
     }
 
-    let text=document.getElementById("text").value;
-    let imgGet=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
-    img.src=imgGet;
-    console.log(text);
+    let text = document.getElementById("text").value;
+
+    if (text == "") {
+        alert("Please Enter text");
+        loader.style.display = "none";
+
+    } else {
+
+        let imgGet = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+        img.src = imgGet;
+        console.log(text);
+    }
 })
 
 
